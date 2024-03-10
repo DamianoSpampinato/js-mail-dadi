c397e5dfde6e732de7153a0573764f837b285024
Damiano <ghgg585@gmail.com>
2024-03-10 17:54:43 +0100

    feat: add random numbers to js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
+// generare due numeri randomici 
+let userNumber = Math.floor(Math.random() * 7);
+let cpuNumber = Math.floor(Math.random() * 7);
+console.log(userNumber);
+console.log(cpuNumber);
+//stampare numeri al click del bottone
 
 
+
+//stabilire quali dei numeri è più grande e stampare risposta
