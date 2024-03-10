26c9b90b5f716912c17286ca4ae90942ac6d4db8
Damiano <ghgg585@gmail.com>
2024-03-10 23:36:34 +0100

    fix emailFound position in js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,31 +1,31 @@
 //registro email
 const emailRegistry = ['example1@email.com', 'example2@email.com', 'mariourbani@gmail.com'];
 console.log(emailRegistry);
-let emailFound = false;
 const newDiv = document.createElement('div')
 let outputMessage;
 
 //button funzion
 const summitButton = document.querySelector('#summit');
 summitButton.addEventListener('click', function(){
+    let emailFound = false;
     //chidere email utente
     let userEmail = document.querySelector('#user-email').value;
     // controllare se è presente nel registro
     for(let i = 0; i < emailRegistry.length; i++) {
         const thisEmail = emailRegistry[i];
-        console.log(thisEmail);
+        
         //se trovo l'email la salvo
         if(thisEmail === userEmail){
             emailFound = true;
         }
         if(emailFound === true) {
             let outputMessage = 'Attendi! log- in incorso';
-            console.log(outputMessage);
+            
             newDiv.innerHTML = (outputMessage);
         } else {
             let outputMessage = 'Utente non registrato! Signuppa';
             newDiv.innerHTML = (outputMessage);
-            console.log(outputMessage);
+          
     }
     
     
