90ba2799fb811d9263065383fd1fde17958a2d98
Damiano <ghgg585@gmail.com>
2024-03-10 18:12:47 +0100

    Feat add event listener to button + print in DOM

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // generare due numeri randomici 
-let userNumber = Math.floor(Math.random() * 7);
-let cpuNumber = Math.floor(Math.random() * 7);
-console.log(userNumber);
-console.log(cpuNumber);
+let userNumber;
+let cpuNumber;
+//creazione div element
+const newDiv = document.createElement('div')
+console.log(newDiv)
 //stampare numeri al click del bottone
+const generatorButton = document.querySelector('#button');
+generatorButton.addEventListener('click', function(){
+    userNumber = Math.floor(Math.random() * 7);
+    cpuNumber = Math.floor(Math.random() * 7);
+    console.log(userNumber);
+    console.log(cpuNumber);
+    newDiv.innerHTML = ('il tuo numero è ' + userNumber + ' il numero della cpu è ' + cpuNumber);
+    document.querySelector('#container').appendChild(newDiv);
+})
 
 
 
